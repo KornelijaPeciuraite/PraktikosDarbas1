b5fd731c29616b6474d3feb8a9375fe3c9a93ca0
Your Name <your.email@example.com>
2024-03-05 23:48:17 +0200

    added change color button

--- app/src/main/java/com/example/praktinisdarbas1/MainActivity.java
+++ app/src/main/java/com/example/praktinisdarbas1/MainActivity.java
@@ -16,15 +16,21 @@ import androidx.core.content.ContextCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView btnchangetext;
+    private TextView btnchangecolor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.btnchangetext = findViewById(R.id.btnchangetext);
+        this.btnchangecolor = findViewById(R.id.btnchangecolor);
     }
 
     public void btntextclick(View view) {
         this.btnchangetext.setText("Hello");
+    }
+
+    public void btnchangecolor(View view) {
+        this.btnchangecolor.setTextColor(getResources().getColor(R.color.red));
     }
 }
