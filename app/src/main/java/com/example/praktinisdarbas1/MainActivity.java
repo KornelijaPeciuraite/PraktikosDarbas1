88246b0d78a2a8fab5832acb5c51d41a64ba55c9
Your Name <your.email@example.com>
2024-03-05 23:42:06 +0200

    added change text button

--- app/src/main/java/com/example/praktinisdarbas1/MainActivity.java
+++ app/src/main/java/com/example/praktinisdarbas1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.praktinisdarbas1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 public class MainActivity extends AppCompatActivity {
 
+    private TextView btnchangetext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnchangetext = findViewById(R.id.btnchangetext);
+    }
+
+    public void btntextclick(View view) {
+        this.btnchangetext.setText("Hello");
     }
 }
